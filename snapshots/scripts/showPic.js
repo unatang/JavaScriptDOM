079bbc89b5d4c14b7201ca961f3e1d821381203b
unatang <1589410802@qq.com>
2016-05-07 13:29:01 +0800

    third commit

--- snapshots/scripts/showPic.js
+++ snapshots/scripts/showPic.js
@@ -6,4 +6,10 @@ function showPic(whichPic)
     var palceholder = document.getElementById("placeholder");
     // 给placeholder里面的src属性设置新的值为 source
     placeholder.setAttribute("src", source);
+    // 选取title属性的值给text
+    var text = whichPic.getAttribute("title");
+    //选取id等于description的元素给变量description
+    var description = document.getElementById("description");
+    //把text的值给description元素的第一个子节点的节点值
+    description.firstChild.nodeValue = text;
 }
